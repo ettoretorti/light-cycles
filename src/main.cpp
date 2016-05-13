1910d32bfe850924d9416b4e96e69442c583a942
Ettore Torti <ettore.torti@student.manchester.ac.uk>
2016-05-13 21:09:52 +0100

    Add pause, escape, and fix whitespace in main

--- src/main.cpp
+++ src/main.cpp
@@ -19,13 +19,13 @@
 
 const char* vshaderSrc = "#version 330\n"
                          ""
-			 "uniform mat4 model;\n"
+                         "uniform mat4 model;\n"
                          "uniform mat4 viewProjection;\n"
                          ""
                          "in vec4 pos;\n"
-			 "in vec4 color;\n"
-			 ""
-			 "out vec4 fColor;\n"
+                         "in vec4 color;\n"
+                         ""
+                         "out vec4 fColor;\n"
                          ""
                          "void main() {\n"
                          "  gl_Position = viewProjection * model * pos;\n"
@@ -34,7 +34,7 @@ const char* vshaderSrc = "#version 330\n"
 
 const char* fshaderSrc = "#version 330\n"
                          ""
-			 "in vec4 fColor;\n"
+                         "in vec4 fColor;\n"
                          "out vec4 color;\n"
                          ""
                          "void main() {\n"
@@ -42,7 +42,7 @@ const char* fshaderSrc = "#version 330\n"
                          "}\n";
 
 static inline double toRad(double degrees) {
-	return degrees * 3.1415926 / 180.0;
+        return degrees * 3.1415926 / 180.0;
 }
 
 
@@ -56,66 +56,66 @@ static inline double toRad(double degrees) {
  * - a valid window is returned
  */
 GLFWwindow* init() {
-	if(!glfwInit()) return nullptr;
+        if(!glfwInit()) return nullptr;
 
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
         glfwWindowHint(GLFW_SAMPLES, 8); //8xMSAA
 
-	GLFWwindow* win = glfwCreateWindow(640, 480, "Light cycles", nullptr, nullptr);
+        GLFWwindow* win = glfwCreateWindow(640, 480, "Light cycles", nullptr, nullptr);
 
-	glfwMakeContextCurrent(win);
-	glfwSwapInterval(1); //vsync
+        glfwMakeContextCurrent(win);
+        glfwSwapInterval(1); //vsync
         glEnable(GL_MULTISAMPLE);
 
-	//make sure we get what we want
-	glewExperimental = GL_TRUE;
-	GLenum glewErr = glewInit();
-	if(glewErr != GLEW_OK) {
-		glfwDestroyWindow(win);
-		return nullptr;
-	}
+        //make sure we get what we want
+        glewExperimental = GL_TRUE;
+        GLenum glewErr = glewInit();
+        if(glewErr != GLEW_OK) {
+                glfwDestroyWindow(win);
+                return nullptr;
+        }
 
-	return win;
+        return win;
 }
 
 void shutdown() {
-	glfwTerminate();
+        glfwTerminate();
 }
 
 void mainloop(GLFWwindow* win, gl::Program& p) {
-	using namespace lcycle;
+        using namespace lcycle;
         using namespace gfx;
         using namespace mathfu;
-        
+
         const vec4 BLUE(0.0, 0.0, 1.0, 1.0);
         const vec4 BLUE_TRAIL(0.0, 0.75, 1.0, 1.0);
         const vec4 RED(1.0, 0.0, 0.0, 1.0);
         const vec4 RED_TRAIL(1.0, 0.4, 0.4, 1.0);
 
-	Cycle p1({-1.0, 0.0}, 3.14159), p2({1.0, 0.0}, 0.0);
-	std::function<CycleInput()> i1 = [win]() -> CycleInput {
-		bool l = glfwGetKey(win, GLFW_KEY_A);
-		bool r = glfwGetKey(win, GLFW_KEY_D);
+        Cycle p1({-1.0, 0.0}, 3.14159), p2({1.0, 0.0}, 0.0);
+        std::function<CycleInput()> i1 = [win]() -> CycleInput {
+                bool l = glfwGetKey(win, GLFW_KEY_A);
+                bool r = glfwGetKey(win, GLFW_KEY_D);
 
-		if(l && r)  return {  0.0 };
-		else if(l)  return { -1.0 };
-		else if(r)  return {  1.0 };
-		else        return {  0.0 };
-	};
-	std::function<CycleInput()> i2 = [win]() -> CycleInput {
-		bool l = glfwGetKey(win, GLFW_KEY_LEFT);
-		bool r = glfwGetKey(win, GLFW_KEY_RIGHT);
+                if(l && r)  return {  0.0 };
+                else if(l)  return { -1.0 };
+                else if(r)  return {  1.0 };
+                else        return {  0.0 };
+        };
+        std::function<CycleInput()> i2 = [win]() -> CycleInput {
+                bool l = glfwGetKey(win, GLFW_KEY_LEFT);
+                bool r = glfwGetKey(win, GLFW_KEY_RIGHT);
 
-		if(l && r)  return {  0.0 };
-		else if(l)  return { -1.0 };
-		else if(r)  return {  1.0 };
-		else        return {  0.0 };
-	};
+                if(l && r)  return {  0.0 };
+                else if(l)  return { -1.0 };
+                else if(r)  return {  1.0 };
+                else        return {  0.0 };
+        };
 
-	World wo(50.0, 0.2, {{p1, i1, RED, RED_TRAIL},  {p2, i2, BLUE, BLUE_TRAIL}});
+        World wo(50.0, 0.2, {{p1, i1, RED, RED_TRAIL},  {p2, i2, BLUE, BLUE_TRAIL}});
         WorldRenderer wr(wo);
-        
+
         mathfu::mat4 mdl = mathfu::mat4::Identity();
         mathfu::mat4 view = mathfu::mat4::LookAt(mathfu::vec3(0.0),
                                                  mathfu::vec3(0.0, 0.0, 1.0),
@@ -130,60 +130,71 @@ void mainloop(GLFWwindow* win, gl::Program& p) {
         glfwGetFramebufferSize(win, &w, &h);
         glViewport(0, 0, w, h);
 
-        double time = glfwGetTime();
-	while(!glfwWindowShouldClose(win)) {
-		glfwPollEvents();
+        bool running  = false;
+        bool pPressed = false;
+        double time   = glfwGetTime();
+        while(!glfwWindowShouldClose(win)) {
+                glfwPollEvents();
+
+                if(glfwGetKey(win, GLFW_KEY_ESCAPE)) {
+                        break;
+                }
+
+                bool pNow = glfwGetKey(win, GLFW_KEY_P);
+                if(!pPressed && pNow) running = !running;
+                pPressed = pNow;
+
                 int nW, nH;
                 glfwGetFramebufferSize(win, &nW, &nH);
                 if(nW != w || nH != h) glViewport(0, 0, w, h);
                 w = nW; h = nH;
 
                 double curTime = glfwGetTime();
-		wo.runFor(curTime - time);
+                if(running) wo.runFor(curTime - time);
                 time = curTime;
 
-		glClear(GL_COLOR_BUFFER_BIT);
-		wr.render();
+                glClear(GL_COLOR_BUFFER_BIT);
+                wr.render();
                 glfwSwapBuffers(win);
-	}
+        }
 }
 
 int main() {
-	using namespace std;
-	using namespace gl;
+        using namespace std;
+        using namespace gl;
 
-	GLFWwindow* win = init();
+        GLFWwindow* win = init();
 
-	Shader vs(GL_VERTEX_SHADER);
-	vs.source(vshaderSrc);
-	if(!vs.compile()) {
-		cerr << "Could not compile vertex shader\n\n" << vs.infoLog() << endl;
-		return -1;
-	}
+        Shader vs(GL_VERTEX_SHADER);
+        vs.source(vshaderSrc);
+        if(!vs.compile()) {
+                cerr << "Could not compile vertex shader\n\n" << vs.infoLog() << endl;
+                return -1;
+        }
 
-	Shader fs(GL_FRAGMENT_SHADER);
-	fs.source(fshaderSrc);
-	if(!fs.compile()) {
-		cerr << "Could not compile fragment shader\n\n" << fs.infoLog() << endl;
-		return -1;
-	}
+        Shader fs(GL_FRAGMENT_SHADER);
+        fs.source(fshaderSrc);
+        if(!fs.compile()) {
+                cerr << "Could not compile fragment shader\n\n" << fs.infoLog() << endl;
+                return -1;
+        }
 
-	Program p;
-	p.setVertexShader(vs);
-	p.setFragmentShader(fs);
-	p.bindAttrib(0, "pos");
-	p.bindAttrib(1, "color");
-	if(!p.link()) {
-		cerr << "Could not link program\n\n" << p.infoLog() << endl;
-		return -1;
-	}
-	p.use();
+        Program p;
+        p.setVertexShader(vs);
+        p.setFragmentShader(fs);
+        p.bindAttrib(0, "pos");
+        p.bindAttrib(1, "color");
+        if(!p.link()) {
+                cerr << "Could not link program\n\n" << p.infoLog() << endl;
+                return -1;
+        }
+        p.use();
 
-	mainloop(win, p);
+        mainloop(win, p);
 
-	glfwDestroyWindow(win);
+        glfwDestroyWindow(win);
 
-	shutdown();
+        shutdown();
 
-	return 0;
+        return 0;
 }
