a0ed05d5217302d062f7d0be41c8295caa2d0792
Ettore Torti <ettore.torti@student.manchester.ac.uk>
2016-05-28 18:51:39 +0100

    Fix indentation and refactor player creation

--- src/main.cpp
+++ src/main.cpp
@@ -18,34 +18,33 @@
 
 #include "gfx/WorldRenderer.hpp"
 
-const char* vshaderSrc = "#version 330\n"
-                         ""
-                         "uniform mat4 model;\n"
-                         "uniform mat4 viewProjection;\n"
-                         ""
-                         "in vec4 pos;\n"
-                         "in vec4 color;\n"
-                         ""
-                         "out vec4 fColor;\n"
-                         ""
-                         "void main() {\n"
-                         "  gl_Position = viewProjection * model * pos;\n"
-                         "  fColor = color;\n"
-                         "}\n";
+const char* vshaderSrc =
+    "#version 330\n"
+    ""
+    "uniform mat4 model;\n"
+    "uniform mat4 viewProjection;\n"
+    ""
+    "in vec4 pos;\n"
+    "in vec4 color;\n"
+    ""
+    "out vec4 fColor;\n"
+    ""
+    "void main() {\n"
+    "  gl_Position = viewProjection * model * pos;\n"
+    "  fColor = color;\n"
+    "}\n";
 
-const char* fshaderSrc = "#version 330\n"
-                         ""
-                         "in vec4 fColor;\n"
-                         "out vec4 color;\n"
-                         ""
-                         "void main() {\n"
-                         "  color = fColor;\n"
-                         "}\n";
+const char* fshaderSrc =
+    "#version 330\n"
+    ""
+    "in vec4 fColor;\n"
+    "out vec4 color;\n"
+    ""
+    "void main() {\n"
+    "  color = fColor;\n"
+    "}\n";
 
-static inline double toRad(double degrees) {
-        return degrees * 3.1415926 / 180.0;
-}
-
+static inline double toRad(double degrees) { return degrees * 3.1415926 / 180.0; }
 
 /*!
  * Preconditions
@@ -57,160 +56,158 @@ static inline double toRad(double degrees) {
  * - a valid window is returned
  */
 GLFWwindow* init(int width, int height) {
-        if(!glfwInit()) return nullptr;
+    if(!glfwInit()) return nullptr;
 
-        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-        glfwWindowHint(GLFW_SAMPLES, 8); //8xMSAA
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_SAMPLES, 8);  // 8xMSAA
 
-        GLFWwindow* win = glfwCreateWindow(width, height, "Light cycles", nullptr, nullptr);
+    GLFWwindow* win = glfwCreateWindow(width, height, "Light cycles", nullptr, nullptr);
 
-        glfwMakeContextCurrent(win);
-        glfwSwapInterval(1); //vsync
-        glEnable(GL_MULTISAMPLE);
+    glfwMakeContextCurrent(win);
+    glfwSwapInterval(1);  // vsync
+    glEnable(GL_MULTISAMPLE);
 
-        //make sure we get what we want
-        glewExperimental = GL_TRUE;
-        GLenum glewErr = glewInit();
-        if(glewErr != GLEW_OK) {
-                glfwDestroyWindow(win);
-                return nullptr;
-        }
+    // make sure we get what we want
+    glewExperimental = GL_TRUE;
+    GLenum glewErr = glewInit();
+    if(glewErr != GLEW_OK) {
+        glfwDestroyWindow(win);
+        return nullptr;
+    }
 
-        return win;
+    return win;
 }
 
 void shutdown() {
-        glfwTerminate();
+    glfwTerminate();
+}
+
+std::function<lcycle::CycleInput()> mkInputFunc(GLFWwindow* win, int lKey, int rKey) {
+    return [win, lKey, rKey]() -> lcycle::CycleInput {
+        bool l = glfwGetKey(win, lKey);
+        bool r = glfwGetKey(win, rKey);
+
+        if(l && r) return {0.0};
+        else if(l) return {-1.0};
+        else if(r) return {1.0};
+        else       return {0.0};
+    };
 }
 
 void mainloop(GLFWwindow* win, gl::Program& p) {
-        using namespace lcycle;
-        using namespace gfx;
-        using namespace mathfu;
+    using namespace lcycle;
+    using namespace gfx;
+    using namespace mathfu;
 
-        const vec4 BLUE(0.0, 0.0, 1.0, 1.0);
-        const vec4 BLUE_TRAIL(0.0, 0.75, 1.0, 1.0);
-        const vec4 RED(1.0, 0.0, 0.0, 1.0);
-        const vec4 RED_TRAIL(1.0, 0.4, 0.4, 1.0);
+    const vec4 P_COLORS[] = {
+        vec4(0.0, 0.0, 1.0, 1.0),  // blue
+        vec4(1.0, 0.0, 0.0, 1.0),  // red
+        vec4(1.0, 1.0, 0.0, 1.0),  // yellow
+    };
 
-        Cycle p1({-1.0, 0.0}, 3.14159), p2({1.0, 0.0}, 0.0);
-        std::function<CycleInput()> i1 = [win]() -> CycleInput {
-                bool l = glfwGetKey(win, GLFW_KEY_A);
-                bool r = glfwGetKey(win, GLFW_KEY_D);
+    const vec4 T_COLORS[] = {
+        vec4(0.0, 0.75, 1.0, 1.0),  // blue
+        vec4(1.0, 0.4, 0.4, 1.0),   // red
+        vec4(1.0, 1.0, 0.5, 1.0),   // yellow
+    };
 
-                if(l && r)  return {  0.0 };
-                else if(l)  return { -1.0 };
-                else if(r)  return {  1.0 };
-                else        return {  0.0 };
-        };
-        std::function<CycleInput()> i2 = [win]() -> CycleInput {
-                bool l = glfwGetKey(win, GLFW_KEY_LEFT);
-                bool r = glfwGetKey(win, GLFW_KEY_RIGHT);
+    Cycle p1({-1.0, 0.0}, 3.14159), p2({1.0, 0.0}, 0.0);
+    std::function<CycleInput()> i1 = mkInputFunc(win, GLFW_KEY_A, GLFW_KEY_D);
+    std::function<CycleInput()> i2 = mkInputFunc(win, GLFW_KEY_LEFT, GLFW_KEY_RIGHT);
 
-                if(l && r)  return {  0.0 };
-                else if(l)  return { -1.0 };
-                else if(r)  return {  1.0 };
-                else        return {  0.0 };
-        };
+    World wo(50.0, 0.2, {{p1, i1, P_COLORS[0], T_COLORS[0]}, {p2, i2, P_COLORS[1], T_COLORS[1]}});
+    WorldRenderer wr(wo);
 
-        World wo(50.0, 0.2, {{p1, i1, RED, RED_TRAIL},  {p2, i2, BLUE, BLUE_TRAIL}});
-        WorldRenderer wr(wo);
+    mathfu::mat4 mdl  = mathfu::mat4::Identity();
+    mathfu::mat4 view = mathfu::mat4::LookAt(mathfu::vec3(0.0), mathfu::vec3(0.0, 0.0, 1.0),
+                                             mathfu::vec3(0.0, 1.0, 0.0), 1.0);
+    mathfu::mat4 proj = mathfu::mat4::Ortho(-30, 30, -30, 30, 0.1, 2.0);
 
-        mathfu::mat4 mdl = mathfu::mat4::Identity();
-        mathfu::mat4 view = mathfu::mat4::LookAt(mathfu::vec3(0.0),
-                                                 mathfu::vec3(0.0, 0.0, 1.0),
-                                                 mathfu::vec3(0.0, 1.0, 0.0), 1.0);
-        mathfu::mat4 proj = mathfu::mat4::Ortho(-30, 30, -30, 30, 0.1, 2.0);
+    glUniformMatrix4fv(p.getUniform("model"), 1, false, &mdl[0]);
+    glUniformMatrix4fv(p.getUniform("viewProjection"), 1, false, &(proj * view)[0]);
 
-        glUniformMatrix4fv(p.getUniform("model"), 1, false, &mdl[0]);
-        glUniformMatrix4fv(p.getUniform("viewProjection"), 1, false, &(proj * view)[0]);
+    int w, h;
+    glfwGetFramebufferSize(win, &w, &h);
+    glViewport(0, 0, w, h);
 
+    bool running  = false;
+    bool pPressed = false;
+    double time = glfwGetTime();
+    while(!glfwWindowShouldClose(win)) {
+        glfwPollEvents();
 
-        int w, h;
-        glfwGetFramebufferSize(win, &w, &h);
-        glViewport(0, 0, w, h);
+        if(glfwGetKey(win, GLFW_KEY_ESCAPE)) { break; }
 
-        bool running  = false;
-        bool pPressed = false;
-        double time   = glfwGetTime();
-        while(!glfwWindowShouldClose(win)) {
-                glfwPollEvents();
+        bool pNow = glfwGetKey(win, GLFW_KEY_P);
+        if(!pPressed && pNow) running = !running;
+        pPressed = pNow;
 
-                if(glfwGetKey(win, GLFW_KEY_ESCAPE)) {
-                        break;
-                }
+        int nW, nH;
+        glfwGetFramebufferSize(win, &nW, &nH);
+        if(nW != w || nH != h) glViewport(0, 0, w, h);
+        w = nW; h = nH;
 
-                bool pNow = glfwGetKey(win, GLFW_KEY_P);
-                if(!pPressed && pNow) running = !running;
-                pPressed = pNow;
+        double curTime = glfwGetTime();
+        if(running) wo.runFor(curTime - time);
+        time = curTime;
 
-                int nW, nH;
-                glfwGetFramebufferSize(win, &nW, &nH);
-                if(nW != w || nH != h) glViewport(0, 0, w, h);
-                w = nW; h = nH;
-
-                double curTime = glfwGetTime();
-                if(running) wo.runFor(curTime - time);
-                time = curTime;
-
-                glClear(GL_COLOR_BUFFER_BIT);
-                wr.render();
-                glfwSwapBuffers(win);
-        }
+        glClear(GL_COLOR_BUFFER_BIT);
+        wr.render();
+        glfwSwapBuffers(win);
+    }
 }
 
 int main(int argc, char** argv) {
-        using namespace std;
-        using namespace gl;
+    using namespace std;
+    using namespace gl;
 
-	int width = 600, height = 600;
-	
-	if(argc >= 3) {
-		try {
-			int w = stoi(argv[1]);
-			int h = stoi(argv[2]);
+    int width = 600, height = 600;
 
-			width  = w;
-			height = h;
-		} catch (...) {
-			cerr << "Could not parse dimensions from first 2 command line args" << endl;
-		}
-	}
+    if(argc >= 3) {
+        try {
+            int w = stoi(argv[1]);
+            int h = stoi(argv[2]);
 
-
-        GLFWwindow* win = init(width, height);
-
-        Shader vs(GL_VERTEX_SHADER);
-        vs.source(vshaderSrc);
-        if(!vs.compile()) {
-                cerr << "Could not compile vertex shader\n\n" << vs.infoLog() << endl;
-                return -1;
+            width  = w;
+            height = h;
+        } catch(...) {
+            cerr << "Could not parse dimensions from first 2 command line args" << endl;
         }
+    }
 
-        Shader fs(GL_FRAGMENT_SHADER);
-        fs.source(fshaderSrc);
-        if(!fs.compile()) {
-                cerr << "Could not compile fragment shader\n\n" << fs.infoLog() << endl;
-                return -1;
-        }
+    GLFWwindow* win = init(width, height);
 
-        Program p;
-        p.setVertexShader(vs);
-        p.setFragmentShader(fs);
-        p.bindAttrib(0, "pos");
-        p.bindAttrib(1, "color");
-        if(!p.link()) {
-                cerr << "Could not link program\n\n" << p.infoLog() << endl;
-                return -1;
-        }
-        p.use();
+    Shader vs(GL_VERTEX_SHADER);
+    vs.source(vshaderSrc);
+    if(!vs.compile()) {
+        cerr << "Could not compile vertex shader\n\n" << vs.infoLog() << endl;
+        return -1;
+    }
 
-        mainloop(win, p);
+    Shader fs(GL_FRAGMENT_SHADER);
+    fs.source(fshaderSrc);
+    if(!fs.compile()) {
+        cerr << "Could not compile fragment shader\n\n" << fs.infoLog() << endl;
+        return -1;
+    }
 
-        glfwDestroyWindow(win);
+    Program p;
+    p.setVertexShader(vs);
+    p.setFragmentShader(fs);
+    p.bindAttrib(0, "pos");
+    p.bindAttrib(1, "color");
+    if(!p.link()) {
+        cerr << "Could not link program\n\n" << p.infoLog() << endl;
+        return -1;
+    }
+    p.use();
 
-        shutdown();
+    mainloop(win, p);
 
-        return 0;
+    glfwDestroyWindow(win);
+
+    shutdown();
+
+    return 0;
 }
