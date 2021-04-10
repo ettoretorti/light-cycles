f913291d38c36b2cc70b0e793d8ee1b365ba4e38
Ettore Sergey Torti <ettoretorti@gmail.com>
2021-04-10 15:36:31 +0200

    Improve GL debug callback logging

--- src/main.cpp
+++ src/main.cpp
@@ -55,8 +55,42 @@ void glfw_error(int error, const char* msg) {
 
 #ifndef NDEBUG
 void APIENTRY gl_error(GLenum src, GLenum type, GLuint id, GLenum severity, GLsizei length, const GLchar* msg, const void* uParam) {
-    (void) src; (void) type; (void) id; (void) severity; (void) length; (void) uParam;
-    std::cerr << msg << std::endl;
+    (void) id; (void) length; (void) uParam;
+    auto srcToStr = [=]() -> const char* {
+        switch (src) {
+            case GL_DEBUG_SOURCE_API: return "SOURCE_API";
+            case GL_DEBUG_SOURCE_WINDOW_SYSTEM: return "WINDOW_SYSTEM";
+            case GL_DEBUG_SOURCE_SHADER_COMPILER: return "SHADER_COMPILER";
+            case GL_DEBUG_SOURCE_THIRD_PARTY: return "THIRD_PARTY";
+            case GL_DEBUG_SOURCE_APPLICATION: return "APPLICATION";
+            case GL_DEBUG_SOURCE_OTHER: return "OTHER";
+            default: return "UNKNOWN";
+        }
+    };
+    auto typeToStr = [=]() -> const char* {
+        switch (type) {
+            case GL_DEBUG_TYPE_ERROR: return "ERROR";
+            case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR: return "DEPRECATED_BEHAVIOR";
+            case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR: return "UNDEFINED_BEHAVIOR";
+            case GL_DEBUG_TYPE_PORTABILITY: return "PORTABILITY";
+            case GL_DEBUG_TYPE_PERFORMANCE: return "PERFORMANCE";
+            case GL_DEBUG_TYPE_MARKER: return "MARKER";
+            case GL_DEBUG_TYPE_PUSH_GROUP: return "PUSH_GROUP";
+            case GL_DEBUG_TYPE_POP_GROUP: return "POP_GROUP";
+            case GL_DEBUG_TYPE_OTHER: return "OTHER";
+            default: return "UNKNOWN";
+        }
+    };
+    auto sevToStr = [=]() -> const char* {
+        switch (severity) {
+            case GL_DEBUG_SEVERITY_HIGH: return "HIGH";
+            case GL_DEBUG_SEVERITY_MEDIUM: return "MEDIUM";
+            case GL_DEBUG_SEVERITY_LOW: return "LOW";
+            case GL_DEBUG_SEVERITY_NOTIFICATION: return "NOTIFICATION";
+            default: return "UNKNOWN";
+        }
+    };
+    std::cerr << "[GL_" << srcToStr() << "][" << typeToStr() << "][" << sevToStr() << "]: " << msg << std::endl;
 }
 #endif
 
